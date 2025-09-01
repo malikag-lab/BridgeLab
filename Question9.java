4c2ee12a52725b9b175c07b02145ce0722bf6d62
Malika <Malika7024.mca@chitkara.edu.in>
2025-09-02 01:15:24 +0530

    Adding Question 9

---
+++ Question9.java
@@ -0,0 +1,13 @@
+public class Question9 {
+ public static void main(String[] args) {
+    int num=10;
+    Integer obj=num;
+
+    System.out.println("Autoboxing: From int to Integer: "+obj);
+
+    Integer obj2=20;
+    int num2=obj2;
+
+    System.out.println("Unboxing: From Integer to int: "+num2);
+ }   
+}
