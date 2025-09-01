738e16523d3505bd80b3d9d0d15ddf88907c61ca
Malika <Malika7024.mca@chitkara.edu.in>
2025-09-01 21:07:43 +0530

    Adding Question 5

---
+++ Question5.java
@@ -0,0 +1,10 @@
+//Write a program to find the ASCII value of a character.
+import java.util.*;
+public class Question5 {
+    public static void main(String[] args) {
+        char charVariable='A';
+        int AsciiVariable= charVariable;
+
+        System.out.println("The Character is:"+charVariable+" "+"and its ASCII value is:"+AsciiVariable);
+    }
+}
